4c4a8cd39e8c69c5b9c4c56ff97337358549f6d8
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-12-29 17:07:20 +0300

    fix(lint) refactor code in files

--- rollup.config.js
+++ rollup.config.js
@@ -5,40 +5,40 @@ import {dts} from 'rollup-plugin-dts';
 import url from '@rollup/plugin-url';
 
 export default [
-    {
-        input: './src/index.ts',
-        output: [
-            {
-                file: './dist/index.js',
-                format: 'cjs',
-            },
-            {
-                file: './dist/index.esm.js',
-                format: 'esm',
-            },
-        ],
-        external: ['react'],
-        plugins: [
-            typescript({
-                tsconfig: './tsconfig.json',
-            }),
-            postcss({
-                extract: 'index.css',
-                modules: true,
-                minimize: true,
-                use: ['sass'],
-            }),
-            terser(),
-            url(),
-        ],
-    },
-    {
-        input: './dist/index.d.ts',
-        output: [{
-            file: './dist/index.d.ts',
-            format: 'esm',
-        }],
-        external: [/\.(css|scss)$/],
-        plugins: [dts()],
-    },
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        file: './dist/index.js',
+        format: 'cjs',
+      },
+      {
+        file: './dist/index.esm.js',
+        format: 'esm',
+      },
+    ],
+    external: ['react'],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+      }),
+      postcss({
+        extract: 'index.css',
+        modules: true,
+        minimize: true,
+        use: ['sass'],
+      }),
+      terser(),
+      url(),
+    ],
+  },
+  {
+    input: './dist/index.d.ts',
+    output: [{
+      file: './dist/index.d.ts',
+      format: 'esm',
+    }],
+    external: [/\.(css|scss)$/],
+    plugins: [dts()],
+  },
 ];
