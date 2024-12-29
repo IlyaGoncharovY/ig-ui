87e7fb1f081aa0e51229b8ac3fd109470f4f2be1
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-12-29 17:10:21 +0300

    fix(button) refactor Button.tsx added JSDoc, and refactor lint.

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import {Meta, StoryFn} from "@storybook/react";
+import React from 'react';
+import {Meta, StoryFn} from '@storybook/react';
 
-import {Button, ButtonProps} from "./Button";
+import {Button, ButtonProps} from './Button';
 
 export default {
-    title: 'components/Button',
-    component: Button,
-    args: {
-        children: 'Click me',
-        disabled: false,
+  title: 'components/Button',
+  component: Button,
+  args: {
+    children: 'Click me',
+    disabled: false,
+  },
+  parameters: {
+    controls: {
+      exclude: [],
     },
-    parameters: {
-        controls: {
-            exclude: []
-        }
-    }
-} as Meta<ButtonStoryProps>
+  },
+} as Meta<ButtonStoryProps>;
 
 type ButtonStoryProps = Pick<ButtonProps, 'className' | 'children'>
 
 export const ButtonStoryTemplate: StoryFn<ButtonStoryProps> = ({...args}) => (
-    <Button {...args} />
+  <Button {...args} />
 );
 
 ButtonStoryTemplate.storyName = 'Button';
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,27 @@
 import React, {ButtonHTMLAttributes, FC} from 'react';
 
-import s from './Button.module.scss';
+import s from './Button.module.css';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
 }
 
+/**
+ * Button компонент для повторного использования элементов пользовательского интерфейса.
+ * @param {React.ReactNode} children - Содержимое, отображаемое внутри кнопки.
+ * @param {string} [className] - Дополнительные классы CSS для стилизации.
+ * @param {ButtonHTMLAttributes<HTMLButtonElement>} props - Дополнительные атрибуты кнопок.
+ * @returns {JSX.Element} Стилизованный компонент кнопки.
+ * @example
+ * // Пример использования:
+ * import { Button } from "goncharov-ui"
+ * <Button onClick={() => console.log('Clicked!')}>Click Me</Button>
+ */
 export const Button: FC<ButtonProps> = ({
   children,
   className = '',
   ...props
-}) => {
+}: ButtonProps): JSX.Element => {
   return (
     <button
       className={`${s.buttonContainer} ${className}`}
